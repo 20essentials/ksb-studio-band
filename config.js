911e111e0c56b9b81ca42acc6177a4750956c42e
DominicDev <s0l0estudi4r@gmail.com>
2025-11-09 22:01:53 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' K.S.B STUDIO';
+export const nameSongs = [
+	"BELIEVE / FOLDER5", /*1*/
+	"AO HARU RIDE SEKAI ", /*2*/
+	"DAN DAN KOKORO", /*3*/
+	"DAKARABOKUHA ONGAKU", /*4*/
+	"MEMORIES", /*5*/
+	"RUROUNI KENSHIN", /*6*/
+	"MYSTERIOUS EYES", /*7*/
+	"HONEYWORKS", /*8*/
+	"MAGIC KAITO", /*9*/
+	"KOI WA AMEAGARI NO YOU NI", /*10*/
+	"SUPERCELL", /*11*/
+	"TADA KIMI NI HARE", /*12*/
+	"CANELE", /*13*/
+	"GURENGE", /*14*/
+	"MEETS TRYSAIL", /*15*/
+	"PRETENDER", /*16*/
+	"STRIMS", /*17*/
+	"BROKEN HEART OF GOLD", /*18*/
+	"SIAMSHADE", /*19*/
+	"DANDAN", /*20*/
+]
